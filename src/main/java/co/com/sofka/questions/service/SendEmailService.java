a4a723a1a70e32fa8914666600edf922cf66fb38
Dayro Martinez <djmd96@hotmail.com>
2022-01-11 17:32:52 -0500

    Servicio notificacion email ok!

---
+++ src/main/java/co/com/sofka/questions/service/SendEmailService.java
@@ -0,0 +1,45 @@
+package co.com.sofka.questions.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@Validated
+public class SendEmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public SendEmailService() {
+    }
+
+    public Mono<String> sendEmail(String to, String subject, String body) {
+
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            message.setSubject(subject);
+            MimeMessageHelper helper;
+            helper = new MimeMessageHelper(message, true);
+            helper.setFrom("djmd96@hotmail.com");
+            helper.setTo(to);
+            helper.setText(body, true);
+            javaMailSender.send(message);
+
+        } catch (MessagingException exception){
+            exception.getMessage();
+        }
+
+        return Mono.just("¡Correo enviado exitosamente!");
+    }
+
+
+}
+
+
--- src/main/java/co/com/sofka/questions/usecases/AddAnswerUseCase.java
+++ src/main/java/co/com/sofka/questions/usecases/AddAnswerUseCase.java
@@ -3,6 +3,8 @@ package co.com.sofka.questions.usecases;
 import co.com.sofka.questions.model.AnswerDTO;
 import co.com.sofka.questions.model.QuestionDTO;
 import co.com.sofka.questions.reposioties.AnswerRepository;
+import co.com.sofka.questions.service.SendEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
@@ -12,14 +14,18 @@ import java.util.Objects;
 @Service
 @Validated
 public class AddAnswerUseCase implements SaveAnswer {
+
+    @Autowired
+    private final SendEmailService sendEmailService;
     private final AnswerRepository answerRepository;
     private final MapperUtils mapperUtils;
     private final GetUseCase getUseCase;
 
-    public AddAnswerUseCase(MapperUtils mapperUtils, GetUseCase getUseCase, AnswerRepository answerRepository) {
+    public AddAnswerUseCase(MapperUtils mapperUtils, GetUseCase getUseCase, AnswerRepository answerRepository, SendEmailService sendEmailService) {
         this.answerRepository = answerRepository;
         this.getUseCase = getUseCase;
         this.mapperUtils = mapperUtils;
+        this.sendEmailService = sendEmailService;
     }
 
     public Mono<QuestionDTO> apply(AnswerDTO answerDTO) {
@@ -28,6 +34,10 @@ public class AddAnswerUseCase implements SaveAnswer {
                 answerRepository.save(mapperUtils.mapperToAnswer().apply(answerDTO))
                         .map(answer -> {
                             question.getAnswers().add(answerDTO);
+                            sendEmailService.sendEmail(
+                                    question.getEmailUser(),
+                                    "Se ha añadido una nueva respuesta a tu pregunta: '" + question.getQuestion()+"'",
+                                    "He aquí la respuesta: '" + answer.getAnswer()+"'. Entra a la web de 'Quién quiere ser Sofkiano' para ver todos los detalles! Te esperamos ;)");
                             return question;
                         })
         );
